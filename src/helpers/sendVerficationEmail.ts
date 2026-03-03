d5baa36f9cc490e24da72183bcef236c1fce4670
Sedow360 <ayushmaiti2004@gmail.com>
2026-03-03 22:24:05 +0530

    Resend fixes

--- src/helpers/sendVerficationEmail.ts
+++ src/helpers/sendVerficationEmail.ts
@@ -1,4 +1,4 @@
-import { resend } from "@/lib/resend";
+import { getResend } from "@/lib/resend";
 import VerificationEmail from "../../email/VerificationEmail";
 import { ApiResponse } from "@/types/ApiResponse";
 
@@ -7,6 +7,7 @@ export async function sendVerificationEmail(
     username: string,
     code: string
 ): Promise<ApiResponse> {
+    const resend = getResend();
     try {
         await resend.emails.send({
                 from: "onboarding@resend.dev",
